42b4b5de49825f465543c807e747657b1a0e4128
mahua20191207 <1370676582@qq.com>
2023-01-18 16:45:28 +0800

    第一次提交

---
+++ src/testgithub.java
@@ -0,0 +1,12 @@
+/**
+ * testgithub class
+ * description: TODO
+ *
+ * @author User
+ * @date 2023/1/18
+ */
+public class testgithub {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
